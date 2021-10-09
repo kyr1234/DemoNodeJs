291220534115bc83107067fffa867b2469cc8a61
kyr1234 <yugkhokhar18@gmail.com>
2021-10-09 23:28:02 +0530

    node

---
+++ index.mjs
@@ -0,0 +1,19 @@
+import { readFile,writeFile} from 'fs/promises';
+
+//Readfile returns an promise
+
+let fileread = await readFile(new URL('index.html', import.meta.url), 'utf-8')
+console.log(fileread)
+
+const data = {
+  title: 'HELLO HOW DO YOU DO',
+  body: 'I AM DOING WEB DEVELOPMENT',
+}
+
+for (const [k, v] of Object.entries(data)) {
+  fileread = fileread.replace(`{${k}}`, v)
+}
+console.log(fileread);
+
+
+await writeFile(new URL("index.html",import.meta.url),fileread);
